00a338c76b1dc0b9bf778e0677184498dd874cdb
jongwonpc <c3392302@naver.com>
2023-04-02 05:50:18 +0900

    countOneByOne
    숫자를 입력 받을 시 각 자리의 숫자를 isNum369를 통해 true값일 때 1씩 더해주는
    count의 합을 리턴한다.

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -10,4 +10,18 @@ public class Problem3 {
     private static boolean isNum369(int num){
         return (num == 3 || num == 6 || num == 9);
     }
+
+    //숫자 입력 받을 시 각 자리의 숫자를 isNum369를 통해 true일때의 count합을 리턴한다.
+    private static int countOneByOne(int num){
+        int count = 0;
+        while(num >=10){
+            if(isNum369(num%10)){
+                count++;
+            }
+            num=num/10;
+        }
+        if(isNum369(num)) count++;
+        return count;
+    }
+
 }
