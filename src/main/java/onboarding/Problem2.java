94c0dd287f0bc9270611f24c316331a5a3846fa3
jongwonpc <c3392302@naver.com>
2023-04-02 00:46:10 +0900

    O(n)은 이전 커밋에 비해 느릴지라도 가독성을 높였다.
    whenContinuousEnd 메서드를 통해 반복이 시작된다면 시작되는 인덱스랑 끝나는
    인덱스를 구해주었다.
    checkStepByStep을 통해 string의 i와 i+1번쨰 인자가 같은지 boolean을
    리턴하는 함수를 생성했다.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,56 +1,28 @@
 package onboarding;
 
-import java.util.Stack;
-
-/*
-암호문을 좋아하는 괴짜 개발자 브라운이 이번에는 중복 문자를 이용한 새로운 암호를 만들었다.
-예를 들어 "browoanoommnaon"이라는 암호문은 다음과 같은 순서로 해독할 수 있다.
-
-"browoanoommnaon"
-"browoannaon"
-"browoaaon"
-"browoon"
-"brown"
-brown
-임의의 문자열 cryptogram이 매개변수로 주어질 때, 연속하는 중복 문자들을 삭제한 결과를
-return 하도록 solution 메서드를 완성하라.
- */
-
-// 하나씩 스택에 넣다가 peek값이 push값과 같다면 다를때까지 pop해준다.
 public class Problem2 {
 
     public static String solution(String cryptogram) {
-        Stack<Character> stack = new Stack<>();
-        int index = 0;
-        Character last = '1';
-        while(index<=cryptogram.length()-1){
-            if(stack.isEmpty() && last != cryptogram.charAt(index) ){
-                stack.push(cryptogram.charAt(index++));
-            }else if (stack.isEmpty() && last == cryptogram.charAt(index)){
-                index++;
-            }else{
-                if(last == cryptogram.charAt(index)){
-                    index++;
-                }else{
-                    if(stack.peek() == cryptogram.charAt(index)){
-                        stack.pop();
-                        last =  cryptogram.charAt(index);
-                        index++;
-
-                    }else{
-                        stack.push(cryptogram.charAt(index++));
-                    }
-                }
-            }
-        }
-        String answer = "";
-        for(char x : stack) answer+=x;
-
-
-        return answer;
+        return "";
     }
 
+    //cryptogram의 i번째 인자와 i+1번째 인자의 값이 같은지 확인해주는 메서드
+    private static boolean checkStepByStep(String cryptogram, int one, int two) {
+        return cryptogram.charAt(one) == cryptogram.charAt(two);
+    }
 
-
+    //반복하는 문자열이 몇 번째 인덱스에서 끝나는지 알려주는 메서드
+    private static int whenContinuousEnd(String cryptogram, int index) {
+        int endIndex = index;
+        for (int i = index; i < cryptogram.length() - 1; i++) {
+            if (checkStepByStep(cryptogram, i, i + 1)) {
+                endIndex++;
+            }
+            if (!checkStepByStep(cryptogram, i, i + 1)) {
+                return endIndex;
+            }
+        }
+        return cryptogram.length() - 1;
+    }
 
 }
