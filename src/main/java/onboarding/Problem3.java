a9d86b7da06ed44bcfc021c06a8f5659a46d8014
jongwonpc <c3392302@naver.com>
2023-04-02 05:41:36 +0900

    isNum369
    num 입력받을 시 해당 숫자가 3,6,9에 해당하는가? 맞다면 true 아니면 false

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -5,4 +5,9 @@ public class Problem3 {
         int answer = 0;
         return answer;
     }
+
+    //해당 숫자가 3,6,9에 해당하는가?
+    private static boolean isNum369(int num){
+        return (num == 3 || num == 6 || num == 9);
+    }
 }
