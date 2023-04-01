8e28f7392f90b71f6db5c2ba30a034078efa3321
jongwonpc <c3392302@naver.com>
2023-04-02 00:49:23 +0900

    replaceString
    whenContinuousEnd를 통해 얻은 반복이 시작되고 끝나는 인덱스 사이를
    빈 값으로 대체해주는 함수

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -6,6 +6,18 @@ public class Problem2 {
         return "";
     }
 
+    //반복이 시작되는 인덱스랑 끝나는 인덱스(whenContinuousEnd로 구한 값)를 통해 그 사이의 값들을 빈 값으로
+    //대체한다.
+    private static String replaceString(String cryptogram) {
+        for (int i = 0; i < cryptogram.length() - 1; i++) {
+            if (checkStepByStep(cryptogram,i,i+1)) {
+                int index = whenContinuousEnd(cryptogram, i) + 1;
+                return new StringBuilder(cryptogram).replace(i, index, "").toString();
+            }
+        }
+        return cryptogram;
+    }
+
     //cryptogram의 i번째 인자와 i+1번째 인자의 값이 같은지 확인해주는 메서드
     private static boolean checkStepByStep(String cryptogram, int one, int two) {
         return cryptogram.charAt(one) == cryptogram.charAt(two);
