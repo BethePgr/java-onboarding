95745b1d682924d98b78a437c8e0e4b73ee17afe
jongwonpc <c3392302@naver.com>
2023-04-02 18:49:48 +0900

    mappingKeyAndValue
    int값이 들어오면 int와 동일한 key에 해당되는 value값을 Character형으로
    변환시켜 리턴하는 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -10,7 +10,8 @@ public class Problem4 {
     //A = 65 Z=90 M = 77  a = 97 z = 122 m= 109
     public static String solution(String word) {
         String answer = "";
-        return answer;
+        completeMap();
+        return "";
     }
 
     //A는 Z로, a는 Z로 역순시켜주는 static map을 완성시키는 메서드를 만든다.
@@ -19,6 +20,12 @@ public class Problem4 {
             map.put(65+i,90-i);
             map.put(97+i,122-i);
         }
+    }
+
+    //들어온 숫자 하나를 map의 키라고 생각했을 때, 해당되는 value값을 Character형으로 가져오는 메서드
+    private static Character mappingKeyAndValue(int num){
+        int intNum = map.get(num);
+        return (char)intNum;
     }
 
 }
