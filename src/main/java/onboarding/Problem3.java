94e188eb47b82ec3f365e605197f799d67e31941
jongwonpc <c3392302@naver.com>
2023-04-02 18:28:13 +0900

    problem3 컨벤션 완료

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,32 +1,35 @@
 package onboarding;
 
 public class Problem3 {
+
     public static int solution(int number) {
         return count369FromOneToNum(number);
     }
 
     //해당 숫자가 3,6,9에 해당하는가?
-    private static boolean isNum369(int num){
+    private static boolean isNum369(int num) {
         return (num == 3 || num == 6 || num == 9);
     }
 
     //숫자 입력 받을 시 각 자리의 숫자를 isNum369를 통해 true일때의 count합을 리턴한다.
-    private static int countOneByOne(int num){
+    private static int countOneByOne(int num) {
         int count = 0;
-        while(num >=10){
-            if(isNum369(num%10)){
+        while (num >= 10) {
+            if (isNum369(num % 10)) {
                 count++;
             }
-            num=num/10;
+            num = num / 10;
         }
-        if(isNum369(num)) count++;
+        if (isNum369(num)) {
+            count++;
+        }
         return count;
     }
 
     //1부터 입력받는 숫자까지 countOneByOne을 통해 이제까지 나온 3,6,9의 총 개수를 구한다.
-    private static int count369FromOneToNum(int num){
+    private static int count369FromOneToNum(int num) {
         int count = 0;
-        for(int i = 1;i<=num;i++){
+        for (int i = 1; i <= num; i++) {
             count += countOneByOne(i);
         }
         return count;
