571fa59de8ca03611e3da21572c77fd53a907cc0
jongwonpc <c3392302@naver.com>
2023-04-02 21:53:28 +0900

    repeatReturnQuoRemainder , solution
    for문을 통해 static int[] arr의 횟수만큼 returnQuoRemainder를 시켜준다.
    solution 완성, 컨벤션 적용 완료

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -15,19 +15,30 @@ public class Problem5 {
 | 15000	| [0, 1, 1, 0, 0, 0, 0, 0, 0] |
      */
 
-    static int [] arr = {50000,10000,5000,1000,500,100,50,10,1};
+    static int[] arr = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
 
     public static List<Integer> solution(int money) {
-        List<Integer> list = new ArrayList();
+        List<Integer> list = repeatReturnQuoRemainder(money);
         return list;
     }
 
     //앞의 숫자가 들어오면 뒷 숫자로 나눈 몫과 나머지를 배열로 리턴한다.
-    private static int[] returnQuoRemainder(int num1,int num2){
-        int [] arr = new int [2];
-        arr[0] = num1/num2;
-        arr[1] = num1%num2;
+    private static int[] returnQuoRemainder(int num1, int num2) {
+        int[] arr = new int[2];
+        arr[0] = num1 / num2;
+        arr[1] = num1 % num2;
         return arr;
     }
 
+    //for문을 통해 static int 배열 arr를 하나하나 returnQuoRemainder를 적용하고 list<Integer>을 반환함.
+    //한 번 할때마다 returnQuoRemainder의 0번지는 list에 추가되고 1번지는 num으로 재할당해준다.
+    private static List<Integer> repeatReturnQuoRemainder(int num) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int[] ints = returnQuoRemainder(num, arr[i]);
+            list.add(ints[0]);
+            num = ints[1];
+        }
+        return list;
+    }
 }
