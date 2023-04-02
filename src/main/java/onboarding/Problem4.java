f9b75d216eec3e8d00f68120a9c4a342e53ce3b8
jongwonpc <c3392302@naver.com>
2023-04-02 18:42:24 +0900

    completeMap()
    전역변수 key랑 value가 Integer인 map을 잡고, for문을 통해 a=z,b=y...
    이런 값을 가지게 만들어주는 메서드

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,24 @@
 package onboarding;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    static Map<Integer,Integer> map = new HashMap<>();
+
+    //A = 65 Z=90 M = 77  a = 97 z = 122 m= 109
     public static String solution(String word) {
         String answer = "";
         return answer;
     }
+
+    //A는 Z로, a는 Z로 역순시켜주는 static map을 완성시키는 메서드를 만든다.
+    private static void completeMap(){
+        for(int i = 0;i<=25;i++){
+            map.put(65+i,90-i);
+            map.put(97+i,122-i);
+        }
+    }
+
 }
