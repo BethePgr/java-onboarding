455780a8f7ee43a02602782780581895247c5b66
jongwonpc <c3392302@naver.com>
2023-04-04 21:08:43 +0900

    friendsWithUser, addFriends
    addFriends : 길이 2인 list가 들어왔을 때, user가 아닌 쪽을 list에
    담는 메서드
    friendsWithUser : friends의 모든 배열에 addFriends을 적용시키는 메서드

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -1,11 +1,37 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Problem7 {
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
-        List<String> answer = Collections.emptyList();
-        return answer;
+
+        return null;
     }
+
+    //user와 직접적인 친구인 목록을 리턴하는 메서드
+    private static List<String> friendsWithUser(String user,List<List<String>> friends){
+        ArrayList<String> friendsWithUserList = new ArrayList<>();
+        for(List<String> friend : friends){
+            if(friend.contains(user)){
+                addFriends(user, friendsWithUserList, friend);
+            }
+        }
+        return friendsWithUserList;
+    }
+
+    //길이가 2인 리스트에서 user가 아닌 다른 한명의 이름을 리스트에 담는 메소드
+    private static void addFriends(String user, ArrayList<String> friendsWithUserList, List<String> friend) {
+        for(String str : friend){
+            if(!str.equals(user)){
+                friendsWithUserList.add(str);
+            }
+        }
+    }
+
+
+
 }
